19881a9b99123441cc287ad62d128761239a6419
Ecem <tugbaecemy@gmail.com>
2023-05-10 16:14:23 +0300

    Sorular eklendi

---
+++ Assets/Scrpits/Soru.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Yeni Soru",menuName ="Sorular/Soru")]
+public class Soru : ScriptableObject
+{
+
+    public new string soruNumarasi;
+    public string soru;
+
+    public string cevap;
+    
+
+
+}//class
